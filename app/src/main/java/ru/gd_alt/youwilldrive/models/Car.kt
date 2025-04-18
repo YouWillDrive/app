671a6682207ce25f53aefbc095bb92b9f0243690
G82ft <leonidkhvan123@gmail.com>
2025-04-19 00:16:11 +0300

    Instructor profile also why Car does not have a model field? fixed it

--- app/src/main/java/ru/gd_alt/youwilldrive/models/Car.kt
+++ app/src/main/java/ru/gd_alt/youwilldrive/models/Car.kt
@@ -3,4 +3,4 @@ package ru.gd_alt.youwilldrive.models
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Car(val id: Int, var plateNumber: String, var color: String, var transmission: Transmission)
+class Car(val id: Int, var model: String, var plateNumber: String, var color: String, var transmission: Transmission)
---
+++ app/src/main/java/ru/gd_alt/youwilldrive/ui/screens/InstructorProfile/IntructorProfile.kt
@@ -0,0 +1,225 @@
+package ru.gd_alt.youwilldrive.ui.screens.InstructorProfile
+
+
+import androidx.compose.animation.VectorConverter
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material.icons.filled.DirectionsCar
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import ru.gd_alt.youwilldrive.R
+import ru.gd_alt.youwilldrive.models.Cadet
+import ru.gd_alt.youwilldrive.models.Car
+import ru.gd_alt.youwilldrive.models.Placeholders.DefaultCadet
+import ru.gd_alt.youwilldrive.models.Placeholders.DefaultCar1
+import ru.gd_alt.youwilldrive.models.Placeholders.DefaultCar2
+import ru.gd_alt.youwilldrive.models.Placeholders.DefaultCar3
+import ru.gd_alt.youwilldrive.models.Placeholders.DefaultCar4
+import ru.gd_alt.youwilldrive.models.User
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun CadetProfileScreen() {
+    Scaffold(
+        Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(stringResource(R.string.profile), fontWeight = FontWeight.Bold)
+                },
+                modifier = Modifier.clip(RoundedCornerShape(0.dp, 0.dp, 20.dp, 20.dp)),
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) {
+        Box(
+            Modifier
+                .padding(it)
+                .fillMaxSize()
+        ) {
+            CadetProfile()
+        }
+    }
+}
+
+@Composable
+fun CadetProfile(
+    cadet: Cadet = DefaultCadet
+) {
+    val user: User = cadet.user
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp)
+    ) {
+        ProfileHeader(user)
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Cars()
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Card(
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+        ) {
+            Row(
+                Modifier.fillMaxWidth().padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    stringResource(R.string.cadets),
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Icon(Icons.Default.ChevronRight,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary)
+            }
+        }
+    }
+}
+
+@Composable
+fun ProfileHeader(user: User) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .size(96.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                Icons.Default.Person,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier.size(64.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.size(16.dp))
+
+        Column {
+            Text(
+                text = user.surname,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = "${user.name} ${user.patronymic}",
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+    }
+}
+
+@Composable
+fun Cars(
+    cars: List<Car> = listOf(DefaultCar1, DefaultCar2, DefaultCar3, DefaultCar4)
+) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        for (i in 0..cars.size / 2) {
+            Row(
+                Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                for (car in cars.slice(i*2..kotlin.math.min(i*2+1, cars.size-1))) {
+                    Card(
+                        Modifier
+                            .weight(1f)
+                            .aspectRatio(1.5f).padding(5.dp),
+                        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
+                        shape = RoundedCornerShape(16.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surface
+                        ),
+                    ) {
+                        Box(
+                            Modifier.fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Row(
+                                Modifier
+                                    .padding(5.dp).fillMaxSize(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Absolute.Left
+                            ) {
+                                Icon(Icons.Default.DirectionsCar,
+                                    contentDescription = null,
+                                    tint = MaterialTheme.colorScheme.primary,
+                                    modifier = Modifier.size(64.dp)
+                                )
+                                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                                    Text(text="${car.model}, ${car.color}", color = MaterialTheme.colorScheme.primary, fontWeight = FontWeight.Bold, textAlign = TextAlign.Center)
+                                    Text(text=car.plateNumber)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
